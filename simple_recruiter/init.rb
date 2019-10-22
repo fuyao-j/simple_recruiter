aec4bb6eac3b100283c701b0c7bf3bb484388651
mr-proxyz <mr.prxz@gmail.com>
2019-10-22 23:36:48 +0300

    Initial commit

---
+++ simple_recruiter/init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :simple_recruiter do
+  name 'Simple Recruiter plugin'
+  author 'Nickolay Marchuk'
+  description "This is a plugin for helps HR with recruitment"
+  version '0.0.1'
+  url 'https://github.com/mr-proxyz/simle_recruiter'
+  author_url 'https://github.com/mr-proxyz'
+end
