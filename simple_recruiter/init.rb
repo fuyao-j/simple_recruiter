8810019f3313cf3bdb510167041f318f6d7ce323
mr-proxyz <mr.prxz@gmail.com>
2019-10-29 00:29:59 +0200

    Add menu button

--- simple_recruiter/init.rb
+++ simple_recruiter/init.rb
@@ -5,4 +5,7 @@ Redmine::Plugin.register :simple_recruiter do
   version '0.0.1'
   url 'https://github.com/mr-proxyz/simle_recruiter'
   author_url 'https://github.com/mr-proxyz'
+
+  menu :application_menu, :vacancies, { controller: 'vacancies', action: 'index' }, caption: 'Vacancies'
+  menu :application_menu, :candidates, { controller: 'candidates', action: 'index' }, caption: 'Candidates'
 end
